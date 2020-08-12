a0d43f4b8bca6c5a1aced6d46020a0bbf1f64225
lvchengfeng <lvchengfeng@haier.com>
2020-08-12 16:36:56 +0800

    hellohttp.go

---
+++ hellohttp.go
@@ -0,0 +1,29 @@
+package main
+
+import(
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"fmt"
+)
+
+var iCnt int = 0;
+
+func helloHandler(w http.ResponseWriter, r * http.Request){
+	iCnt ++;
+	str := "Hello World! friend(" + strconv.Itoa(iCnt) + ")"
+	io.WriteString(w, str)
+	fmt.Println(str)
+}
+
+func main(){
+	ht := http.HandlerFunc(helloHandler)
+	if ht != nil{
+		http.Handle("/hello", ht)
+	}
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil{
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}
